b080033cb5826a17ed9de2a044e1e64fbb57c8c4
wwan13 <wwan13@naver.com>
2024-06-02 19:50:34 +0900

    feat : A class containing all exception handlers

---
+++ src/main/java/io/wwan13/dispatchersorvlet/sorvlet/ExceptionHandlers.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.wwan13.dispatchersorvlet.sorvlet;
+
+import io.wwan13.dispatchersorvlet.sorvlet.exceptioinhandler.DefaultExceptionHandler;
+
+import java.util.List;
+
+public record ExceptionHandlers(
+        List<ExceptionHandler> handlers
+) {
+
+    private static final ExceptionHandler DEFAULT_EXCEPTION_HANDLER = ExceptionHandler.of(
+            new DefaultExceptionHandler(),
+            DefaultExceptionHandler.class.getDeclaredMethods()[0]
+    );
+
+    public ExceptionHandler handlerMapping(Exception exception) {
+        return handlers.stream()
+                .filter(handler -> handler.matches(exception))
+                .findFirst()
+                .orElse(DEFAULT_EXCEPTION_HANDLER);
+    }
+}

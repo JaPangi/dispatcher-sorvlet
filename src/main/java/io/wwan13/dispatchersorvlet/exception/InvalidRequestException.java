7d4a23ce00927731bade3d2d45e4c54ca37a22d3
wwan13 <wwan13@naver.com>
2024-06-02 14:32:41 +0900

    fix : Add getter method to read error message

--- src/main/java/io/wwan13/dispatchersorvlet/exception/InvalidRequestException.java
+++ src/main/java/io/wwan13/dispatchersorvlet/exception/InvalidRequestException.java
@@ -18,11 +18,16 @@ package io.wwan13.dispatchersorvlet.exception;
 
 public class InvalidRequestException extends RuntimeException {
 
-    private static final String DEFAULT_MESSAGE = "Invalid Request";
+    private static final String DEFAULT_MESSAGE = "Invalid request";
 
     private final String message;
 
     public InvalidRequestException() {
         this.message = DEFAULT_MESSAGE;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
